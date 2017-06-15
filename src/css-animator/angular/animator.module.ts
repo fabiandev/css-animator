0a9e3be0773a009eaa17d8856b070cb8fa11455f
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2017-06-15 22:56:46 +0200

    Renamed modules to angular.

---
+++ src/css-animator/angular/animator.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { AnimatesDirective } from './animates.directive';
+
+@NgModule({
+  declarations: [AnimatesDirective]
+})
+export class AnimatorModule { }
