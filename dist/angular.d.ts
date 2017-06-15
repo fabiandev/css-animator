50ce831a8d2af1ca842adcd3c4040170e54dd9b0
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2017-06-15 22:55:33 +0200

    Remove dist from git.

--- dist/angular.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './angular/animator.module';
-export * from './angular/animates.directive';
-export * from './angular/animation.service';
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './builder';
-export * from './angular';
-export * from './contracts';
