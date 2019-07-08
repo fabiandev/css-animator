ef09fb9a4bbeba04f4feb006cdfa8266b94c50eb
Fabian Pirklbauer <fabian.pirklbauer@bitmovin.com>
2019-07-08 16:54:55 +0200

    Update demo

--- docs/angular.main.ts
+++ docs/angular.main.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 import 'zone.js';
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './angular.module';
 
+enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
